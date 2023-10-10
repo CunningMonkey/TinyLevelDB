cda583b35474661cc533d7b7a275bd6a899ea00d
dingtengfei01 <dingtengfei01@baidu.com>
2023-10-10 18:03:41 +0800

    initialized

--- build/tests/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ninja/Projects/LockFreeSkipList/third_party/googletest
-# Build directory: /Users/ninja/Projects/LockFreeSkipList/build/tests/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- build/tests/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ninja/Projects/LockFreeSkipList/third_party/googletest/googletest
-# Build directory: /Users/ninja/Projects/LockFreeSkipList/build/tests/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
