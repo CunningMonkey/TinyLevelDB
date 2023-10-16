f3eed5682c141b5e65080dbfa4409014948c49f8
wukong <wukong86372@qq.com>
2023-10-16 22:21:37 +0800

    使用leveldb的风格重构了一下skiplist，不过去除了迭代器，目前通过共享锁保证并发

--- src/SkipList.h
+++ src/SkipList.h
@@ -5,74 +5,86 @@
 #include <cassert>
 #include <iostream>
 #include <mutex>
+#include <shared_mutex>
+#include <vector>
+#include "util/arena.h"
+#include "util/random.h"
 
 const int MAX_LEVEL = 5;
 
-template <typename Key, typename Value, typename Comparator>
+template <typename Key, typename Comparator>
 class SkipList
 {
 private:
     struct Node
     {
         Key key;
-        Value value;
         Node *nexts[MAX_LEVEL];
 
-        Node(Key key, Value value) : key(key), value(value)
+        Node(Key key) : key(key)
         {
             for (int i = 0; i < MAX_LEVEL; ++i)
             {
                 nexts[i] = nullptr;
             }
         }
+
+        Node *Next(int n)
+        {
+            assert(n >= 0);
+            return nexts[n];
+        }
     };
 
     Node _head;
-    Node _tail;
+    // Node _tail;
     Comparator _comparator;
-    std::mutex _mutex;
+    std::shared_mutex _mutex;
+    Arena *const _arena;
+    Random _rand;
 
 public:
-    SkipList(const Comparator &cmp = Comparator())
-        : _head(Node(0, 0)), _tail(Node(0, 0)), _comparator(cmp)
+    SkipList(const Comparator &cmp, Arena *arena)
+        : _arena(arena), _head(Node(0)), _comparator(cmp), _rand(0xdeadbeef)
     {
         std::srand(std::time(nullptr));
-        for (int i = 0; i < MAX_LEVEL; ++i)
-        {
-            _head.nexts[i] = &_tail;
-        }
     }
 
     ~SkipList() {}
 
-    Value *get(const Key &key);
-    void put(const Key &key, const Value &value);
-
-    bool remove(const Key &key);
+    Key *Get(const Key &key);
+    void Put(const Key &key);
+    bool Contains(const Key &key);
+    Node *FindLast();
 
     void print();
+    std::vector<Key *> iterate();
 
 private:
     int randomLevel()
     {
-        int lvl = 1;
-        while ((std::rand() & 1) && lvl < MAX_LEVEL)
+        // Increase height with probability 1 in kBranching
+        static const unsigned int kBranching = 4;
+        int height = 1;
+        while (height < MAX_LEVEL && _rand.OneIn(kBranching))
         {
-            lvl++;
+            height++;
         }
-        return lvl;
+        assert(height > 0);
+        assert(height <= MAX_LEVEL);
+        return height;
     }
 
-    Node *findLessThanKey(const Key &key, Node* prevs[MAX_LEVEL])
+    Node *findLessThan(const Key &key, Node *prevs[MAX_LEVEL])
     {
         int level = MAX_LEVEL;
         Node *n = _head.nexts[level - 1];
         Node *prev = &_head;
-        for (int level = MAX_LEVEL-1; level >= 0; --level)
+        for (int level = MAX_LEVEL - 1; level >= 0; --level)
         {
             while (true)
             {
-                if (n == &_tail ||  _comparator(n->key, key) >= 0)
+                if (n == nullptr || _comparator(n->key, key) >= 0)
                 {
                     n = prev;
                     prevs[level] = n;
@@ -84,87 +96,96 @@ private:
         }
         return n;
     }
+
+    Node *newNode(const Key &key);
 };
 
-template <typename Key, typename Value, typename Comparator>
-Value *SkipList<Key, Value, Comparator>::get(const Key &key)
+template <typename Key, typename Comparator>
+Key *SkipList<Key, Comparator>::Get(const Key &key)
 {
-    std::lock_guard<std::mutex> lock(_mutex);
-    Node* prevs[MAX_LEVEL];
-    Node *n = findLessThanKey(key, prevs);
+    std::shared_lock<std::shared_mutex> lock(_mutex);
+    Node *prevs[MAX_LEVEL];
+    Node *n = findLessThan(key, prevs);
     Node *next = n->nexts[0];
-    assert(next != nullptr);
-    if (next == &_tail) {
+    if (next == nullptr)
+    {
         return nullptr;
     }
-    int tmp = _comparator(next->key, key);
-    if (tmp == 0)
-    {
-        return &next->value;
-    }
-    return nullptr;
+    return &(next->key);
 }
 
-template <typename Key, typename Value, typename Comparator>
-void SkipList<Key, Value, Comparator>::put(const Key &key, const Value &value)
+template <typename Key, typename Comparator>
+void SkipList<Key, Comparator>::Put(const Key &key)
 {
-    std::lock_guard<std::mutex> lock(_mutex);
-    Node* prevs[MAX_LEVEL];
-    Node *n = findLessThanKey(key, prevs);
+    std::lock_guard<std::shared_mutex> lock(_mutex);
+    Node *prevs[MAX_LEVEL];
+    Node *n = findLessThan(key, prevs);
     Node *next = n->nexts[0];
-    assert(next != nullptr);
-    if (next != &_tail && _comparator(next->key, key) == 0) {
-        next->value = value;
-        return;
-    }
 
     int level = randomLevel();
-    std::unique_ptr<Node> new_node(new Node(key, value));
+
+    auto new_node = newNode(key);
+
     for (int i = 0; i < level; ++i)
     {
         next = prevs[i]->nexts[i];
-        prevs[i]->nexts[i] = new_node.get();
+        prevs[i]->nexts[i] = new_node;
         new_node->nexts[i] = next;
     }
-
-    new_node.release();
 }
 
-template <typename Key, typename Value, typename Comparator>
-bool SkipList<Key, Value, Comparator>::remove(const Key &key)
+template <typename Key, typename Comparator>
+inline bool SkipList<Key, Comparator>::Contains(const Key &key)
 {
-    std::lock_guard<std::mutex> lock(_mutex);
-    Node* prevs[MAX_LEVEL];
-    Node *n = findLessThanKey(key, prevs);
-    Node *next = n->nexts[0];
-    assert(next != nullptr);
-    if (next == &_tail) {
-        return false;
-    }
+    return !(Get(key) == nullptr);
+}
 
-    if (_comparator(next->key, key) == 0)
+template <typename Key, typename Comparator>
+typename SkipList<Key, Comparator>::Node *SkipList<Key, Comparator>::FindLast()
+{
+    Node *n = &_head;
+    while (n->nexts[0] != nullptr)
     {
-        for (int level = 0; level < MAX_LEVEL; ++level) {
-            if (next->nexts[level] != nullptr) {
-                prevs[level]->nexts[level] = next->nexts[level];
-            }
-        }
-        delete next;
-        return true;
+        n = n->nexts[0];
     }
-    return false;
+    return n;
 }
 
-template <typename Key, typename Value, typename Comparator>
-void SkipList<Key, Value, Comparator>::print()
+template <typename Key, typename Comparator>
+void SkipList<Key, Comparator>::print()
 {
-    for (int i = MAX_LEVEL - 1; i >= 0; --i) {
+    for (int i = MAX_LEVEL - 1; i >= 0; --i)
+    {
         std::cout << "head-";
-        Node* n = _head.nexts[i];
-        while(n && n != &_tail) {
-            std::cout << n->key << ":" << n->value << "--";
+        Node *n = _head.nexts[i];
+        while (n && n != nullptr)
+        {
+            std::cout << n->key << "--";
             n = n->nexts[i];
         }
         std::cout << "tail" << std::endl;
     }
+}
+
+template <typename Key, typename Comparator>
+std::vector<Key *> SkipList<Key, Comparator>::iterate()
+{
+    std::vector<Key *> keys;
+    Node *current = _head.Next(0);
+
+    while (current)
+    {
+        keys.push_back(&current->key);
+        current = current->Next(0);
+    }
+
+    return keys;
+}
+
+template <typename Key, typename Comparator>
+typename SkipList<Key, Comparator>::Node *SkipList<Key, Comparator>::newNode(
+    const Key &key)
+{
+    char *_nodememory = _arena->AllocateAligned(sizeof(Node));
+    return new (_nodememory) Node(key);
 }
