ed11c0821e0c3e94f5f5b12ebf6e1597f419e8a0
wukong <wukong86372@qq.com>
2023-11-16 23:34:43 +0800

    加入Table

--- include/table_builder.h
+++ include/table_builder.h
@@ -1,15 +1,24 @@
 #pragma once
 #include <fstream>
 #include "iterator.h"
+#include "slice.h"
 
 void BuildTable(Iterator *it, const char *filename);
 
 class TableBuilder
 {
 public:
-    void Add(const char *key, const char *value);
+    void Add(const Slice &key, const Slice &value);
+    void Finish();
 
-    TableBuilder(const char *filename) : _table_file(filename, std::ios::app) {}
+    TableBuilder(const char *filename) : _table_file(filename, std::ios::app), _count(0)
+    {
+    }
+
+    ~TableBuilder()
+    {
+        _table_file.close();
+    }
 
 private:
     std::ofstream _table_file;
--- src/table_builder.cpp
+++ src/table_builder.cpp
@@ -1,11 +1,31 @@
 #include "table_builder.h"
 
-void BuildTable(Iterator *iter, const char *f)
+void BuildTable(Iterator *iter, const char *filename)
 {
     iter->SeekToFirst();
+    TableBuilder *builder = new TableBuilder(filename);
+
     while (iter->Valid())
     {
+        builder->Add(iter->Key(), iter->Value());
 
         iter->Next();
     }
+}
+
+void TableBuilder::Add(const Slice &key, const Slice &value)
+{
+    // Write key size and key
+    uint32_t key_size = key.size();
+    _table_file.write((const char *)&key_size, sizeof(key_size));
+    _table_file.write(key.data(), key_size);
+
+    // Write value size and value
+    uint32_t value_size = value.size();
+    _table_file.write((const char *)&value_size, sizeof(value_size));
+    _table_file.write(value.data(), value_size);
+}
+
+void TableBuilder::Finish()
+{
 }
